a9ca716675841fae093043831a681cc48307c06b
asabon <asabon.t@gmail.com>
2023-12-21 06:35:17 +0900

    modify

--- build_all.bat
+++ build_all.bat
@@ -1,29 +1,33 @@
 @echo off
 
-set targetdir=.
-set targetprefix=Sample_
-for %%d in (%targetprefix%*) do (
-    echo === %%d ===
-rem    pushd %%d
-rem        mkdir build
-rem        pushd build
-rem            cmake ../
-rem            msbuild %%d.sln
-rem        popd
-rem    popd
-)
+pushd %~dp0
 
-echo === 2nd ===
-pushd Sample_01
-    mkdir build
-    pushd build
-        cmake ../
-        msbuild Sample_01.sln
-        if not exists Debug/out.exe (
-            echo build error
-            exit /b 1
-        )
-        rem 実行するとエラーになってしまうのでひとまずコメントアウト
-        rem Debug/out.exe
+    set targetdir=.
+    set targetprefix=Sample_
+    for /d %%d in (%targetprefix%*) do (
+        echo === %%d ===
+    rem    pushd %%d
+    rem        mkdir build
+    rem        pushd build
+    rem            cmake ../
+    rem            msbuild %%d.sln
+    rem        popd
+    rem    popd
+    )
+
+    echo === 2nd ===
+    pushd Sample_01
+        mkdir build
+        pushd build
+            cmake ../
+            msbuild Sample_01.sln
+            if not exists Debug/out.exe (
+                echo build error
+                exit /b 1
+            )
+            rem 実行するとエラーになってしまうのでひとまずコメントアウト
+            rem Debug/out.exe
+        popd
     popd
+
 popd
