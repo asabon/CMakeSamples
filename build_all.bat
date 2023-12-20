1b354b5d65210813c3e9a2a5d288ae2b9cad3ed5
asabon <asabon.t@gmail.com>
2023-12-20 20:06:29 +0900

    modify build_all.bat

--- build_all.bat
+++ build_all.bat
@@ -3,5 +3,11 @@
 set targetdir=.
 set targetprefix=Sample_
 for %%d in (%targetdir%\%targetprefix%*) do (
-    echo %%d
+    echo === %%d ===
+    pushd %%d
+        mkdir build
+        pushd build
+            cmake ../
+        popd
+    popd
 )
