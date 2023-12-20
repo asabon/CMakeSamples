27397b91878c237a75f2f1a0c6bba32b040bfd05
asabon <asabon.t@gmail.com>
2023-12-21 06:38:23 +0900

    modify

--- build_all.bat
+++ build_all.bat
@@ -1,33 +1,21 @@
 @echo off
 
 pushd %~dp0
-
-    set targetdir=.
     set targetprefix=Sample_
     for /d %%d in (%targetprefix%*) do (
         echo === %%d ===
-    rem    pushd %%d
-    rem        mkdir build
-    rem        pushd build
-    rem            cmake ../
-    rem            msbuild %%d.sln
-    rem        popd
-    rem    popd
-    )
-
-    echo === 2nd ===
-    pushd Sample_01
-        mkdir build
-        pushd build
-            cmake ../
-            msbuild Sample_01.sln
-            if not exists Debug/out.exe (
-                echo build error
-                exit /b 1
-            )
-            rem 実行するとエラーになってしまうのでひとまずコメントアウト
-            rem Debug/out.exe
+        pushd %%d
+            mkdir build
+            pushd build
+                cmake ../
+                msbuild %%d.sln
+                if not exists Debug/out.exe (
+                    echo build error
+                    exit /b 1
+                )
+                rem 実行するとエラーになってしまうのでひとまずコメントアウト
+                rem Debug/out.exe
+            popd
         popd
-    popd
-
+    )
 popd
